94e2aea70e9375e29057586d5961ff756f7716cc
WANZEINT <wanze_intl@outlook.com>
2026-07-05 21:43:16 +0800

    Add Home component with basic layout

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      padding: 40,
+      background: "#000",
+      color: "#fff",
+      minHeight: "100vh"
+    }}>
+      <h1 style={{color:"#d4af37"}}>
+        WANZE INTERNATIONAL
+      </h1>
+      <p>Premium Bath & Wellness Products</p>
+    </div>
+  );
+}
