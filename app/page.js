574a472cc164300a50b030ede76b97104b0096d1
WANZEINT <wanze_intl@outlook.com>
2026-07-05 22:45:08 +0800

    Create Home component for main page

---
+++ app/page.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div style={{
+      padding: 60,
+      minHeight: "100vh",
+      background: "#000"
+    }}>
+      <h1 style={{ color: "#d4af37", fontSize: 40 }}>
+        WANZE INTERNATIONAL
+      </h1>
+
+      <p style={{ marginTop: 20 }}>
+        Premium Bath & Wellness Supplier
+      </p>
+    </div>
+  );
+}
